7b854bca962df776ad3cfe51e4d83338326a4e04
Arnav Goel <arnav.goel1@outlook.com>
2024-01-03 21:59:27 +0530

    Added mode switcher

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignUpPage";
 import MainPage from "./pages/MainPage";
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/" element={<MainPage />} />
-    </Routes>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </>
   );
 }
 export default App;
