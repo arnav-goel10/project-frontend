a3e91f6398d87a4505c51ce80d6a5119f036d97c
Arnav Goel <arnav.goel1@outlook.com>
2024-01-03 23:06:44 +0530

    Added Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import logo from "../assets/logo.jpg";
+import { Box, Grid } from "@mui/material";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <Grid item>
+      <Box
+        display="flex"
+        alignItems="center"
+        marginLeft={"30px"}
+        marginTop={"10px"}
+      >
+        <img src={logo} width={"50px"} />
+        <Grid color="#FF4500" fontWeight={"600"} fontSize={40} marginLeft={2}>
+          Cookit
+        </Grid>
+      </Box>
+    </Grid>
+  );
 };
 
 export default NavBar;
