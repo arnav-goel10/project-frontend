033e56b6e7ad4957d7641d815b0886f3f92343cc
Arnav Goel <arnav.goel1@outlook.com>
2024-01-03 21:48:45 +0530

    Added Navbar Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignUpPage";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
+      <Route path="/" element={<MainPage />} />
     </Routes>
   );
 }
