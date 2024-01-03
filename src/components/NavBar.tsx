341c0180b797365e18085f2ab4fe4c665f1e78dc
Arnav Goel <arnav.goel1@outlook.com>
2024-01-03 23:40:25 +0530

    fixed navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../assets/logo.jpg";
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import { DarkModeSwitch } from "react-toggle-dark-mode";
 
 const NavBar = () => {
@@ -9,32 +9,40 @@ const NavBar = () => {
   const toggleDarkMode = (checked: boolean) => {
     setDarkMode(checked);
   };
-
   return (
-    <Grid item>
-      <Box
-        display="flex"
-        justifyContent="space-between"
-        alignItems="center"
-        marginLeft={"30px"}
-        marginTop={"0"} // Potentially adjust this or parent margins
-      >
-        <Box display="flex" alignItems="center">
-          <img src={logo} width={"50px"} />
-          <Grid color="#FF4500" fontWeight={"600"} fontSize={40} marginLeft={2}>
-            Cookit
-          </Grid>
-        </Box>
-        <Grid marginRight={"30px"} marginTop={"2rem"}>
-          <DarkModeSwitch
-            style={{ marginBottom: "2rem" }}
-            checked={isDarkMode}
-            onChange={toggleDarkMode}
-            size={40}
-          />
-        </Grid>
-      </Box>
-    </Grid>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginTop: 15,
+        marginLeft: 15,
+        marginRight: 15,
+      }}
+    >
+      <div style={{ display: "flex" }}>
+        <img src={logo} width="70" alt="Logo" />
+        <Typography
+          style={{
+            marginLeft: 30,
+            fontSize: 45,
+            height: 20,
+            color: "orangered",
+            fontFamily: "fantasy",
+          }}
+        >
+          Cookit
+        </Typography>
+      </div>
+      <div>
+        <DarkModeSwitch
+          style={{}}
+          checked={isDarkMode}
+          onChange={toggleDarkMode}
+          size={60}
+        />
+      </div>
+    </div>
   );
 };
 
