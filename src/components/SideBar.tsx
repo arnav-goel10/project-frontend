65ddaf5c548a7120b2af8e4fb30c7b22713f6731
Arnav Goel <arnav.goel1@outlook.com>
2024-01-04 23:21:44 +0530

    Added SideBar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import { ListSubheader } from "@mui/material";
+import SideBarListElement from "./SideBarListElement"; // Import the new component
 
-const SideBar = () => {
-  return <div>SideBar</div>;
-};
+export default function SelectedListItem() {
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
 
-export default SideBar;
+  const handleListItemClick = (index: number) => {
+    setSelectedIndex(index);
+  };
+
+  // Example data for list items
+  const cuisines = [
+    "Indian",
+    "Chinese",
+    "Italian",
+    "Japanese",
+    "Mexican",
+    "Spanish",
+    "Thai",
+    "Vietnamese",
+    "French",
+    "Greek",
+    "Korean",
+    "Lebanese",
+    "American",
+    "British",
+    "Caribbean",
+    "Irish",
+    "Mediterranean",
+    "Nordic",
+    "German",
+    "Eastern European",
+    "Jewish",
+  ];
+
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        maxWidth: 360,
+        bgcolor: "background.paper",
+        paddingTop: "3rem",
+        paddingLeft: "2rem",
+        paddingRight: "4rem",
+        fontFamily: "fantasy",
+      }}
+    >
+      <ListSubheader sx={{ fontSize: "1.5rem" }}>Cuisines</ListSubheader>
+      <List component="nav">
+        {cuisines.map((cuisine, index) => (
+          <SideBarListElement
+            key={index}
+            label={cuisine}
+            selected={selectedIndex === index}
+            onClick={() => handleListItemClick(index)}
+          />
+        ))}
+      </List>
+    </Box>
+  );
+}
---
+++ src/components/SideBarListElement.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+
+interface SideBarListElementProps {
+  label: string;
+  icon?: React.ReactNode; // Optional icon for the list item
+  selected?: boolean;
+  onClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+}
+
+const SideBarListElement: React.FC<SideBarListElementProps> = ({
+  label,
+  icon,
+  selected,
+  onClick,
+}) => {
+  return (
+    <ListItemButton selected={selected} onClick={onClick}>
+      {icon && <ListItemIcon>{icon}</ListItemIcon>}
+      <ListItemText primary={label} />
+    </ListItemButton>
+  );
+};
+
+export default SideBarListElement;
