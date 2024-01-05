487b8920d93a84a2922f3c8e4e378827dc294289
Arnav Goel <arnav.goel1@outlook.com>
2024-01-05 12:42:43 +0530

    fixed spacing

--- src/components/HeaderMainPage.tsx
+++ src/components/HeaderMainPage.tsx
@@ -8,7 +8,17 @@ interface Props {
 
 const HeaderMainPage: React.FC<Props> = ({ selectedCuisine }) => {
   return (
-    <Box sx={{ display: "flex", alignItems: "center" }}>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        backgroundColor: "lightgrey",
+        padding: "2rem",
+        borderRadius: "2rem",
+        marginLeft: "0.5rem",
+        marginRight: "5rem",
+      }}
+    >
       <Avatar
         src="https://www.blueosa.com/wp-content/uploads/2020/01/the-best-top-10-indian-dishes.jpg"
         sx={{ width: 100, height: 100 }}
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { Grid } from "@mui/material";
+import { Grid, useTheme } from "@mui/material";
 import SideBar from "../components/SideBar";
 import HeaderMainPage from "../components/HeaderMainPage";
 
 const MainPage: React.FC = () => {
+  const theme = useTheme();
   const [selectedCuisine, setSelectedCuisine] = useState<string>();
 
   const handleCuisineSelect = (cuisine: string) => {
@@ -18,6 +19,9 @@ const MainPage: React.FC = () => {
 
       <Grid item xs={7} sx={{ mt: "2rem" }}>
         <HeaderMainPage selectedCuisine={selectedCuisine} />
+      </Grid>
+      <Grid item xs={2}>
+        Filter
       </Grid>
     </Grid>
   );
