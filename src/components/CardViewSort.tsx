c8d48907be048dfd5af4cb080c933a92dd32aa3c
Arnav Goel <arnav.goel1@outlook.com>
2024-01-05 14:05:00 +0530

    added icons

--- src/components/CardViewSort.tsx
+++ src/components/CardViewSort.tsx
@@ -2,10 +2,11 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
+import { SvgIcon } from "@mui/material";
 
-const CardViewSort = () => {
+const NewTopSort = () => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [dashboardContent, setDashboardContent] = React.useState("Spacious");
+  const [dashboardContent, setDashboardContent] = React.useState("New"); // Initial selection
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
@@ -13,12 +14,38 @@ const CardViewSort = () => {
 
   const handleMenuItemClick = (clickedItem: string) => {
     setDashboardContent(clickedItem);
-    setAnchorEl(null);
+    setAnchorEl(null); // Close menu when item is clicked
   };
 
   const handleClose = () => {
     setAnchorEl(null);
   };
+
+  const newIcon = (
+    <svg
+      fill="currentColor"
+      height="16"
+      icon-name="view-card-outline"
+      viewBox="0 0 20 20"
+      width="16"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M17.882 1H2.118A1.12 1.12 0 0 0 1 2.119v15.762A1.119 1.119 0 0 0 2.118 19h15.764A1.12 1.12 0 0 0 19 17.881V2.119A1.12 1.12 0 0 0 17.882 1Zm-.132 16.75H2.25v-7.138h15.5v7.138ZM2.25 9.362V2.25h15.5v7.112H2.25Z"></path>
+    </svg>
+  );
+
+  const topIcon = (
+    <svg
+      fill="currentColor"
+      height="16"
+      icon-name="view-card-outline"
+      viewBox="0 0 20 20"
+      width="16"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M17.882 1H2.118A1.12 1.12 0 0 0 1 2.119v15.762A1.119 1.119 0 0 0 2.118 19h15.764A1.12 1.12 0 0 0 19 17.881V2.119A1.12 1.12 0 0 0 17.882 1Zm-.132 16.75H2.25v-7.138h15.5v7.138ZM2.25 9.362V2.25h15.5v7.112H2.25Z"></path>
+    </svg>
+  );
 
   return (
     <div>
@@ -31,6 +58,8 @@ const CardViewSort = () => {
         sx={{ color: "orangered" }}
       >
         {dashboardContent}
+
+        {dashboardContent === "New" ? newIcon : topIcon}
       </Button>
       <Menu
         id="basic-menu"
@@ -41,15 +70,15 @@ const CardViewSort = () => {
           "aria-labelledby": "basic-button",
         }}
       >
-        <MenuItem onClick={() => handleMenuItemClick("Spacious")}>
-          Spacious
+        <MenuItem onClick={() => handleMenuItemClick("New")}>
+          {newIcon} New
         </MenuItem>
-        <MenuItem onClick={() => handleMenuItemClick("Compact")}>
-          Compact
+        <MenuItem onClick={() => handleMenuItemClick("Top")}>
+          {topIcon} Top
         </MenuItem>
       </Menu>
     </div>
   );
 };
 
-export default CardViewSort;
+export default NewTopSort;
