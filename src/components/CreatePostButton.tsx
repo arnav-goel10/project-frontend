ba1820b28c454ad00518d0aafc22e2d5d4a00691
Arnav Goel <arnav.goel1@outlook.com>
2024-01-06 16:28:30 +0530

    fixed spacing

--- src/components/CreatePostButton.tsx
+++ src/components/CreatePostButton.tsx
@@ -5,12 +5,12 @@ const CreatePostButton = () => {
   return (
     <Button
       variant="outlined"
-      startIcon={<AddCircleOutlineIcon />}
+      startIcon={<AddCircleOutlineIcon sx={{ fontSize: 40 }} />}
       sx={{
         color: "orangered",
         border: "1px solid orangered",
         padding: "0.8rem 1.6rem",
-        borderRadius: "5rem",
+        borderRadius: "1.5rem",
         fontSize: "1.15rem",
       }}
     >
