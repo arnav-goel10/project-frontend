60357c585f33ae0ff5f5d4db59d270c3dc1d85a2
Arnav Goel <arnav.goel1@outlook.com>
2024-01-10 01:30:46 +0530

    fixing structure of file

--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -1,6 +1,6 @@
 import { Typography } from "@mui/material";
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.png";
+import logo from "../../assets/logo.png";
 
 const Logo = () => {
   return (
